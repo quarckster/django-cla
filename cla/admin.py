cb069ac8a8b5729120365aa2e3c2c25d79c4e726
Dmitry Misharov <dmitry@openssl.org>
2025-07-17 14:18:49 +0200

    change admin site header and title

--- cla/admin.py
+++ cla/admin.py
@@ -4,5 +4,14 @@ from .models import CCLA
 from .models import ICLA
 
 
-admin.site.register(ICLA)
+admin.site.site_header = "OpenSSL CLA Admin"
+admin.site.site_title = "OpenSSL CLA Admin Portal"
+admin.site.index_title = "Welcome to OpenSSL CLA Portal"
+
+
 admin.site.register(CCLA)
+
+@admin.register(ICLA)
+class ICLAModelAdmin(admin.ModelAdmin):
+    list_display = ("email", "full_name", "signed_at", "is_volunteer")
+

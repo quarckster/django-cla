ad4c7d474c054210b54f406d5d47452924be53b8
Dmitry Misharov <dmitry@openssl.org>
2025-10-01 16:03:23 +0200

    move admin site settings to base/wsgi.py

--- base/wsgi.py
+++ base/wsgi.py
@@ -1,7 +1,13 @@
 import os
 
+from django.conf import settings
+from django.contrib import admin
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings")
 
 application = get_wsgi_application()
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+admin.site.site_title = settings.ADMIN_SITE_TITLE
+admin.site.index_title = settings.ADMIN_SITE_INDEX_TITLE
--- cla/admin.py
+++ cla/admin.py
@@ -1,14 +1,8 @@
-from django.conf import settings
 from django.contrib import admin
 
 from .models import CCLA
 from .models import CCLAAttachment
 from .models import ICLA
-
-
-admin.site.site_header = settings.ADMIN_SITE_HEADER
-admin.site.site_title = settings.ADMIN_SITE_TITLE
-admin.site.index_title = settings.ADMIN_SITE_INDEX_TITLE
 
 
 @admin.register(ICLA)
